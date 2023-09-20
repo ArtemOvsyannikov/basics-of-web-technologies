fae9f2974ad477044d57b71c5e01a0769e079ff8
sh1ro <artemrotfront@gmail.com>
2023-09-20 07:44:32 +0300

    update function.html and function.js

--- function.js
+++ function.js
@@ -1,14 +1,21 @@
-const node_for_click = document.getElementById("for_click")
+const node_for_click_all = document.getElementById("for_click_all")
+const node_for_click_first_p = document.getElementById("for_click_first_p")
 
-function find_edit(){
+function find_edit_all(){
     const p1  = document.getElementsByTagName('p')[1]
     console.log(p1.innerText)
-    p1.innerText = 'новое значение'
+    p1.innerText = 'new value for 1st paragraph'
 
     const p2  = document.getElementsByTagName('p')[2]
     console.log(p2.innerText)
-    p2.innerText = 'Для второго параграфа'
+    p2.innerText = 'new value for 2nd paragraph'
+}
+
+function find_edit_first_p(){
+    const first_p = document.getElementsByTagName('p')[1]
+    first_p.innerText = 'new value only for 1st paragraph'
 
 }
 
-node_for_click.addEventListener("click",find_edit)
+node_for_click_all.addEventListener("click",find_edit_all)
+node_for_click_first_p.addEventListener("click", find_edit_first_p)
